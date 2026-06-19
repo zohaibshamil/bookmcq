d41b76f6476ee7e5ca7eaa2e9d5d22d5edc49e87
zohaibshamil <zohaibshamil@gmail.com>
2026-06-19 20:43:35 +0500

    next.config.js

--- next.config.js
+++ next.config.js
@@ -2,24 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
-  
-  async rewrites() {
-    return [
-      { source: '/', destination: '/' },
-      { source: '/quiz', destination: '/quiz' },
-      { source: '/practice', destination: '/practice' },
-      { source: '/contact', destination: '/contact' },
-      { source: '/privacy', destination: '/privacy' },
-    ];
-  },
-  
-  async redirects() {
-    return [
-      { source: '/home', destination: '/', permanent: true },
-      { source: '/index', destination: '/', permanent: true },
-      { source: '/index.html', destination: '/', permanent: true },
-    ];
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
