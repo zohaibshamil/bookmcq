623970bb8bbdbaf8c4542ff3327882dae0d3c052
zohaibshamil <zohaibshamil@gmail.com>
2026-06-19 22:13:09 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-12 text-center text-white/60 text-xs md:text-sm py-6">
-      <p>&copy; 2025 BookMCQ. All rights reserved. | Master Every Chapter</p>
+      <p>&copy; 2025 BookMCQ. All rights reserved. | Master Every Chapter | One Paper MCQ | Govt & Private Job Preparation</p>
     </footer>
   )
 }
