7e4cddeeb926ae2257907b5754b4ba4d3e7c488b
zohaibshamil <zohaibshamil@gmail.com>
2026-06-19 20:39:09 +0500

    Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,29 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
-import Toast from './Toast'
+import { useEffect } from 'react'
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    // Security: Disable right-click
+    document.addEventListener('contextmenu', (e) => e.preventDefault())
+    
+    // Disable keyboard shortcuts
+    document.addEventListener('keydown', (e) => {
+      const forbidden = ['PrintScreen', 'F12', 
+        (e.ctrlKey && e.shiftKey && e.key === 'I'),
+        (e.ctrlKey && e.key === 'u'),
+        (e.ctrlKey && e.key === 's')]
+      if (forbidden.includes(e.key) || forbidden.some(cond => cond === true)) {
+        e.preventDefault()
+      }
+    })
+  }, [])
+
   return (
     <div className="max-w-6xl mx-auto px-4">
       <Navbar />
       <main>{children}</main>
       <Footer />
-      <Toast />
     </div>
   )
 }
