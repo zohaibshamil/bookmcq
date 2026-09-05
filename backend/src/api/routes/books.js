21e98e76dd699b72751bf6879a6a21bfa752096e
zohaibshamil <zohaibshamil@gmail.com>
2026-09-05 15:25:36 +0500

    Create books.js

---
+++ backend/src/api/routes/books.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const booksController = require('../controllers/booksController');
+
+router.get('/', booksController.getAllBooks);
+router.get('/:id', booksController.getBookById);
+router.get('/:id/chapters', booksController.getBookChapters);
+
+module.exports = router;
